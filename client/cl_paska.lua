f6400e7e0fe0af1c9313fdac426b98829c268ab3
jaffamies <46383800+jaffamies@users.noreply.github.com>
2021-02-18 18:39:33 +0200

    Add files via upload

---
+++ client/cl_paska.lua
@@ -0,0 +1,96 @@
+ESX = nil
+
+local Vehicles = {}
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
+Citizen.CreateThread(function()
+	local blip = AddBlipForCoord(vector3(-199.19, -1381.24, 31.26))
+
+	SetBlipSprite(blip, 596)
+	SetBlipScale(blip, 1.0)
+	SetBlipColour(blip, 4)
+	SetBlipDisplay(blip, 4)
+	SetBlipAsShortRange(blip, true)
+
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentString("Check Car Tunings")
+	EndTextCommandSetBlipName(blip)
+
+
+end)
+
+Citizen.CreateThread(function(data)
+while true do
+Wait(0)
+local ped = GetPlayerPed(-1)
+local pos = GetEntityCoords(ped)
+local dist = GetDistanceBetweenCoords(pos.x, pos.y, pos.z, -199.19, -1381.24, 31.26, true)
+
+
+		if dist < 7.8 and IsPedInAnyVehicle(ped, false) then
+	DrawMarker(36, -199.19, -1381.24, 31.26, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0, 2.0, 255, 128, 0, 255, true, true, 2, nil, nil, true)
+	
+	if dist < 2.8 then
+	
+	ESX.ShowHelpNotification('Press ~INPUT_CONTEXT~ to check car tunings')
+	
+	if IsControlJustPressed(0, 38) then
+	
+	
+	local vehicle = GetVehiclePedIsIn(ped)
+	local vehProps = ESX.Game.GetVehicleProperties(vehicle)
+	local model       = GetEntityModel(vehicle)
+	local modelName 	= GetDisplayNameFromVehicleModel(model)
+	local modCount = GetNumVehicleMods(vehicle, modType)
+	
+	vehProps.modEngine = vehProps.modEngine + 1
+	vehProps.modBrakes = vehProps.modBrakes + 1
+	vehProps.modArmor = vehProps.modArmor + 1
+	vehProps.modSuspension = vehProps.modSuspension + 1
+	vehProps.modTransmission = vehProps.modTransmission + 1
+	
+	TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 30000, text = 'Ajoneuvon malli: ' ..modelName })
+	TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 30000, text = 'Jarrujen taso: ' .. tostring(vehProps.modBrakes).. '/' .. GetNumVehicleMods(vehicle, 12) })
+	TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 30000, text = 'Moottorin taso: ' .. tostring(vehProps.modEngine).. '/' .. GetNumVehicleMods(vehicle,11) })
+	TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 30000, text = 'Armorin taso: ' .. tostring(vehProps.modArmor).. '/' .. GetNumVehicleMods(vehicle, 16) })
+	TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 30000, text = 'Jousituksen taso: ' .. tostring(vehProps.modSuspension).. '/' .. GetNumVehicleMods(vehicle,15)})
+	TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 30000, text = 'Vaihteiston taso: ' .. tostring(vehProps.modTransmission).. '/' .. GetNumVehicleMods(vehicle,13)})
+	TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 30000, text = 'Turbo: ' .. tostring(vehProps.modTurbo)})
+
+				end
+			end
+		end
+	end
+end)
+
+RegisterNetEvent('katsotuunaukset')
+AddEventHandler('katsotuunaukset', function()
+local ped = GetPlayerPed(-1)
+
+	local vehicle = GetVehiclePedIsIn(ped)
+	local vehProps = ESX.Game.GetVehicleProperties(vehicle)
+	local model       = GetEntityModel(vehicle)
+	local modelName 	= GetDisplayNameFromVehicleModel(model)
+	local modCount = GetNumVehicleMods(vehicle, modType)
+
+	vehProps.modEngine = vehProps.modEngine + 1
+	vehProps.modBrakes = vehProps.modBrakes + 1
+	vehProps.modArmor = vehProps.modArmor + 1
+	vehProps.modSuspension = vehProps.modSuspension + 1
+	vehProps.modTransmission = vehProps.modTransmission + 1
+	
+	TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 30000, text = 'Ajoneuvon malli: ' ..modelName })
+	TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 30000, text = 'Jarrujen taso: ' .. tostring(vehProps.modBrakes).. '/' .. GetNumVehicleMods(vehicle, 12) })
+	TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 30000, text = 'Moottorin taso: ' .. tostring(vehProps.modEngine).. '/' .. GetNumVehicleMods(vehicle,11) })
+	TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 30000, text = 'Armorin taso: ' .. tostring(vehProps.modArmor).. '/' .. GetNumVehicleMods(vehicle, 16) })
+	TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 30000, text = 'Jousituksen taso: ' .. tostring(vehProps.modSuspension).. '/' .. GetNumVehicleMods(vehicle,15)})
+	TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 30000, text = 'Vaihteiston taso: ' .. tostring(vehProps.modTransmission).. '/' .. GetNumVehicleMods(vehicle,13)})
+	TriggerEvent('mythic_notify:client:SendAlert', { type = 'inform', length = 30000, text = 'Turbo: ' .. tostring(vehProps.modTurbo)})
+		
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Check car tunings'
+
+author 'Jaffamies#1157'
+
+version '1.0.0'
+
+server_scripts { 'server/main.lua', '@mysql-async/lib/MySQL.lua'}
+
+client_script '**/cl_*.lua'
